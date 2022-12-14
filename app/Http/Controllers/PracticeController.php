dc1ffab91c46bd64b88d054a0a49f45c4890ea50
Keyl0ve <kyo.cat34@gmail.com>
2022-12-15 01:59:25 +0900

    fix

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,17 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-use App\Models\User;
-
 class PracticeController extends Controller
 {
-    /**
-     * 指定ユーザーのプロファイルを表示
-     *
-     * @param  int  $id
-     * @return \Illuminate\View\View
-     */
     public function sample()
     {
         return response('practice');
@@ -21,12 +12,6 @@ class PracticeController extends Controller
     public function sample2()
     {
         $test = 'practice2';
-        return response($test);
-    }
-
-    public function sample3()
-    {
-        $test = 'test';
         return response($test);
     }
 }
